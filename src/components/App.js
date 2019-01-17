e370b8c596ce430baa21bb7c473b13cef39f3808
DanielOhn <ohndaniel@gmail.com>
2019-01-17 01:37:40 -0700

    Add portfolio project

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`
+
+function App() {
+  return ( 
+    <Content>
+      <h1>Hello</h1>
+    </Content>
+  )
+}
+
+export default App;
